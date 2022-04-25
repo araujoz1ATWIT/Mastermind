859a6c65e17fe6fafd1ddea327e7c4e840e6396b
Zaki Araujo <araujoz1@wit.edu>
2022-04-25 06:04:29 -0400

    Delete Mastermind directory

--- Mastermind/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module wit.comp1050.mastermind {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens wit.comp1050.mastermind to javafx.fxml;
-    exports wit.comp1050.mastermind;
-}
--- Mastermind/src/main/java/wit/comp1050/mastermind/Levels.java
+++
@@ -1,5 +0,0 @@
-package wit.comp1050.mastermind;
-
-public enum Levels {
-    Novice, Beginner, Intermediate, Expert
-}
