b69694c1835a124c9246ca94308f5828c4fa12e8
Zaki Araujo <araujoz1@wit.edu>
2022-04-25 21:57:58 -0400

    Delete Mastermind directory

--- Mastermind/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module wit.comp1050.mastermind {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens wit.comp1050.mastermind to javafx.fxml;
-    exports wit.comp1050.mastermind;
-}
--- Mastermind/src/main/java/wit/comp1050/mastermind/PegColor.java
+++
@@ -1,7 +0,0 @@
-package wit.comp1050.mastermind;
-
-public enum PegColor {
-    BLUE, GREEN, PURPLE, RED, YELLOW, TRANSPARENT, WHITE
-}
-
-
